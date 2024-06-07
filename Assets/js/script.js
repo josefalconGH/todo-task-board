0139d980dd5c9595435313b57b099a19f2e4f95c
unknown <jfalconpriv@gmail.com>
2024-06-07 18:47:06 -0500

    added task form mock-up and modal to index.html

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,37 +2,37 @@
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
-// Todo: create a function to generate a unique task id
+// TODO: create a function to generate a unique task id
 function generateTaskId() {
-
+    
 }
 
-// Todo: create a function to create a task card
+// TODO: create a function to create a task card
 function createTaskCard(task) {
 
 }
 
-// Todo: create a function to render the task list and make cards draggable
+// TODO: create a function to render the task list and make cards draggable
 function renderTaskList() {
 
 }
 
-// Todo: create a function to handle adding a new task
+// TODO: create a function to handle adding a new task
 function handleAddTask(event){
 
 }
 
-// Todo: create a function to handle deleting a task
+// TODO: create a function to handle deleting a task
 function handleDeleteTask(event){
 
 }
 
-// Todo: create a function to handle dropping a task into a new status lane
+// TODO: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
 
 }
 
-// Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
+// TODO: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
 
 });
